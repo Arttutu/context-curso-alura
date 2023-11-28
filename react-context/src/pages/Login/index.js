4de9ce8d3d66daae205c1f65a94b4b7e3d5cab3e
Arttutu <arthurtt08@gmail.com>
2023-11-28 10:07:28 -0300

    aula 1.1

--- react-context/src/pages/Login/index.js
+++ react-context/src/pages/Login/index.js
@@ -1,50 +1,39 @@
-import { Button } from '@material-ui/core';
-import {
-  Container,
-  Titulo,
-  InputContainer
-} from './styles';
-import {
-  Input,
-  InputLabel,
-  InputAdornment 
-} from '@material-ui/core';
+import { Button } from "@material-ui/core"
+import { Container, Titulo, InputContainer } from "./styles"
+import { Input, InputLabel, InputAdornment } from "@material-ui/core"
+import { useHistory } from "react-router-dom"
 
-function Login() {
+function Login({ nome, saldo, setNome, setSaldo }) {
+  const history = useHistory()
   return (
     <Container>
-      <Titulo>
-        Insira o seu nome
-      </Titulo>
+      <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
-        <InputLabel>
-          Nome
-        </InputLabel>
+        <InputLabel>Nome</InputLabel>
         <Input
           type="text"
+          onChange={(e) => setNome(e.target.value)}
+          value={nome}
         />
       </InputContainer>
       <InputContainer>
-        <InputLabel>
-          Saldo
-        </InputLabel>
+        <InputLabel>Saldo</InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          type="number"
+          onChange={(e) => setSaldo(e.target.value)}
+          value={saldo}
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+        />
       </InputContainer>
       <Button
         variant="contained"
         color="primary"
+        onClick={() => history.push("/feira")}
       >
         Avançar
       </Button>
     </Container>
   )
-};
+}
 
-export default Login;
+export default Login
--- react-context/src/router.js
+++ react-context/src/router.js
@@ -3,17 +3,24 @@ import {
   Route,
   Switch,
 } from "react-router-dom/cjs/react-router-dom.min"
-import React from "react"
+import React, { useState } from "react"
 import Login from "pages/Login"
 import Carrinho from "pages/Carrinho"
 import Feira from "pages/Feira"
 
-export default function router() {
+export default function Router() {
+  const [nome, setNome] = useState("testeNome")
+  const [saldo, setSaldo] = useState("123")
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <Login />
+          <Login
+            nome={nome}
+            setNome={setNome}
+            saldo={saldo}
+            setSaldo={setSaldo}
+          />
         </Route>
         <Route path="/carrinho">
           <Carrinho />
